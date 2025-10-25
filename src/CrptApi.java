c2d4d5c07cdec0cc1d4f5548d3bce20f625b6549
eugene <zhenyavolodin7@gmail.com>
2025-10-25 16:21:01 +0300

    second

--- src/CrptApi.java
+++ src/CrptApi.java
@@ -1,7 +1,7 @@
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+//import com.fasterxml.jackson.annotation.JsonInclude;
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.io.IOException;
 import java.net.URI;
